ec7127e8919ebd45f7fa60115ac9626d5384271e
Cristian Lopez <clopez@knomadmood.com>
2024-07-06 14:06:31 +0200

    Main index page implemented and basic movies page sketch

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home'
+import Movies from './components/Movies';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true, element: <Home />
+      },
+      {
+        path: "/movies",
+        element: <Movies/>,
+      }
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
